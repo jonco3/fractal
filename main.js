caf55c94fe391253774e9dc7557e95df2840de47
Jon Coppeard <jonco@pobox.com>
2017-03-04 16:38:32 +0000

    Split plotter code from interface code

--- main.js
+++ main.js
@@ -4,20 +4,6 @@
 let canvas;
 let canvasScale;
 
-let idleWorkers = [];
-let busyWorkers = [];
-
-let startTime;
-let totalPixels;
-
-let regionQueue = [];
-
-function error(message)
-{
-    alert(message);
-    throw message;
-}
-
 function init()
 {
     params = {
@@ -43,7 +29,7 @@ function init()
     updateCoordsScale();
     updateHistoryState();
     updateFormFromParams();
-    createWorkers();
+    initPlotter(canvas, setStatusPlotting, setStatusFinished);
     resizeCanvas();
 }
 
@@ -121,85 +107,6 @@ function updateFormFromParams()
     form.elements["threads"].value = params.threads;
 }
 
-function createWorkers()
-{
-    if (!window.Worker)
-        error("Requires workers -- please upgrade your browser");
-
-    assert(busyWorkers.length == 0,
-           "createWorkers expects no workers to be running");
-
-    assert(typeof params.threads === 'number', "Bad thread count parameter");
-
-    while (idleWorkers.length > params.threads) {
-        idleWorkers.pop().terminate();
-    }
-
-    for (let i = idleWorkers.length; i < params.threads; i++) {
-        worker = new Worker("worker.js");
-        if (!worker)
-            error("Failed to create worker");
-        worker.onmessage = processMessageFromWorker;
-        idleWorkers.push(worker);
-    }
-
-    var testBuffer = new ArrayBuffer(1);
-    idleWorkers[0].postMessage(["test", testBuffer], [testBuffer]);
-    if (testBuffer.byteLength)
-        error("Requires Transferables -- please upgrade your browser");
-}
-
-function noteWorkerIdle(worker)
-{
-    let i = busyWorkers.indexOf(worker);
-    assert(i !== -1, "Worker not running");
-    busyWorkers.splice(i, 1);
-    idleWorkers.push(worker);
-}
-
-function getIdleWorker()
-{
-    assert(idleWorkers.length > 0, "All workers already running");
-    let worker = idleWorkers.pop();
-    busyWorkers.push(worker);
-    return worker;
-}
-
-function processMessageFromWorker(event)
-{
-    switch(event.data[0]) {
-    case "error":
-        error(event.data[1]);
-        break;
-    case "plotRegionFinished":
-        noteWorkerIdle(this);
-        assert(event.data.length === 4, "Bad response from plotRegion");
-        let region = event.data[1];
-        let buffer = event.data[2];
-        let pixels = event.data[3];
-        plotRegionFinished(region, buffer, pixels);
-        break;
-    default:
-        error("Unrecognised reply from worker: " +
-              JSON.stringify(event.data));
-    }
-}
-
-function plotRegionOnWorker(region)
-{
-    getIdleWorker().postMessage(["plotRegion", params, region]);
-}
-
-function maybeCancelWorkers()
-{
-    regionQueue = [];
-    busyWorkers.forEach((worker) => {
-        worker.terminate();
-    })
-    busyWorkers = [];
-    createWorkers();
-}
-
 function resizeCanvas()
 {
     let container = document.getElementById("container");
@@ -262,89 +169,6 @@ function zoomAt(px, py)
     plotImage();
 }
 
-function plotImage()
-{
-    // Break up the image into (mostly) square tiles and queue the regions to be
-    // plotted.
-
-    const minTileSidePixels = 200;
-    const maxTilesPerSide = 4;
-
-    maybeCancelWorkers();
-    setStatusPlotting();
-
-    let pw = params.image.width;
-    let ph = params.image.height;
-
-    // Calculate number of tiles per side.
-    let pm = Math.min(pw, ph);
-    let tps = Math.floor(pm / minTileSidePixels);
-    tps = Math.max(tps, 1);
-    tps = Math.min(tps, maxTilesPerSide);
-
-    // Calculate tile size.
-    let ts = Math.floor(pm / tps);
-    let nx = Math.floor(pw / ts);
-    let ny = Math.floor(ph / ts);
-
-    assert(regionQueue.length === 0, "Region queue should be empty");
-    for (let sy = 0; sy < ny; sy++) {
-        let y0 = sy * ts;
-        let y1 = (sy + 1) * ts;
-        if (sy === ny - 1)
-            y1 = ph;
-        for (let sx = 0; sx < nx; sx++) {
-            let x0 = sx * ts;
-            let x1 = (sx + 1) * ts;
-            if (sx === nx - 1)
-                x1 = pw;
-            regionQueue.push([x0, y0, x1, y1]);
-        }
-    }
-
-    totalPixels = 0;
-    startTime = performance.now();
-    dispatchWorkers();
-}
-
-function dispatchWorkers()
-{
-    while (idleWorkers.length !== 0 && regionQueue.length !== 0)
-        plotRegionOnWorker(regionQueue.shift());
-}
-
-function plotRegionFinished(region, buffer, pixels)
-{
-    let [x0, y0, x1, y1] = region;
-    assert(x1 > x0 && y1 > y0, "plotRegionFinished got bad region");
-
-    let pw = x1 - x0;
-    let ph = y1 - y0;
-    assert(buffer.byteLength === pw * ph * 4, "Bad buffer size");
-
-    dispatchWorkers();
-
-    let colourData = new Uint8ClampedArray(buffer);
-
-    let image;
-    try {
-        image = new ImageData(colourData, pw, ph);
-    } catch (e) {
-        // This may not work in IE.
-        error("ImageData constructor not supported in your browser: " + e);
-    }
-
-    let context = canvas.getContext("2d");
-    context.putImageData(image, x0, y0);
-
-    totalPixels += pixels;
-
-    if (busyWorkers.length === 0) {
-        let endTime = performance.now();
-        setStatusFinished(totalPixels, endTime - startTime);
-    }
-}
-
 function setStatusPlotting()
 {
     let status = document.getElementById("status");
@@ -378,9 +202,4 @@ function setStatusFinished(pixels, time)
 
     let status = document.getElementById("status");
     status.textContent = elems.join(" ");
-}
-
-function setPlotter(name)
-{
-    params.plotter = name;
 }
---
+++ plotter.js
@@ -0,0 +1,189 @@
+/*-*- Mode: JS; tab-width: 4 -*-*/
+
+let plotterCanvas;
+let plotterStartCallback;
+let plotterEndCallback;
+
+let idleWorkers = [];
+let busyWorkers = [];
+
+let startTime;
+let totalPixels;
+
+let regionQueue = [];
+
+function initPlotter(canvas, startCallback, endCallback)
+{
+    plotterCanvas = canvas;
+    plotterStartCallback = startCallback;
+    plotterEndCallback = endCallback;
+    createWorkers();
+}
+
+function error(message)
+{
+    alert(message);
+    throw message;
+}
+
+function createWorkers()
+{
+    if (!window.Worker)
+        error("Requires workers -- please upgrade your browser");
+
+    assert(busyWorkers.length == 0,
+           "createWorkers expects no workers to be running");
+
+    assert(typeof params.threads === 'number', "Bad thread count parameter");
+
+    while (idleWorkers.length > params.threads) {
+        idleWorkers.pop().terminate();
+    }
+
+    for (let i = idleWorkers.length; i < params.threads; i++) {
+        worker = new Worker("worker.js");
+        if (!worker)
+            error("Failed to create worker");
+        worker.onmessage = processMessageFromWorker;
+        idleWorkers.push(worker);
+    }
+
+    var testBuffer = new ArrayBuffer(1);
+    idleWorkers[0].postMessage(["test", testBuffer], [testBuffer]);
+    if (testBuffer.byteLength)
+        error("Requires Transferables -- please upgrade your browser");
+}
+
+function noteWorkerIdle(worker)
+{
+    let i = busyWorkers.indexOf(worker);
+    assert(i !== -1, "Worker not running");
+    busyWorkers.splice(i, 1);
+    idleWorkers.push(worker);
+}
+
+function getIdleWorker()
+{
+    assert(idleWorkers.length > 0, "All workers already running");
+    let worker = idleWorkers.pop();
+    busyWorkers.push(worker);
+    return worker;
+}
+
+function processMessageFromWorker(event)
+{
+    switch(event.data[0]) {
+    case "error":
+        error(event.data[1]);
+        break;
+    case "plotRegionFinished":
+        noteWorkerIdle(this);
+        assert(event.data.length === 4, "Bad response from plotRegion");
+        let region = event.data[1];
+        let buffer = event.data[2];
+        let pixels = event.data[3];
+        plotRegionFinished(region, buffer, pixels);
+        break;
+    default:
+        error("Unrecognised reply from worker: " +
+              JSON.stringify(event.data));
+    }
+}
+
+function plotRegionOnWorker(region)
+{
+    getIdleWorker().postMessage(["plotRegion", params, region]);
+}
+
+function maybeCancelWorkers()
+{
+    regionQueue = [];
+    busyWorkers.forEach((worker) => {
+        worker.terminate();
+    })
+    busyWorkers = [];
+    createWorkers();
+}
+
+function plotImage()
+{
+    // Break up the image into (mostly) square tiles and queue the regions to be
+    // plotted.
+
+    const minTileSidePixels = 200;
+    const maxTilesPerSide = 4;
+
+    plotterStartCallback();
+    maybeCancelWorkers();
+
+    let pw = params.image.width;
+    let ph = params.image.height;
+
+    // Calculate number of tiles per side.
+    let pm = Math.min(pw, ph);
+    let tps = Math.floor(pm / minTileSidePixels);
+    tps = Math.max(tps, 1);
+    tps = Math.min(tps, maxTilesPerSide);
+
+    // Calculate tile size.
+    let ts = Math.floor(pm / tps);
+    let nx = Math.floor(pw / ts);
+    let ny = Math.floor(ph / ts);
+
+    assert(regionQueue.length === 0, "Region queue should be empty");
+    for (let sy = 0; sy < ny; sy++) {
+        let y0 = sy * ts;
+        let y1 = (sy + 1) * ts;
+        if (sy === ny - 1)
+            y1 = ph;
+        for (let sx = 0; sx < nx; sx++) {
+            let x0 = sx * ts;
+            let x1 = (sx + 1) * ts;
+            if (sx === nx - 1)
+                x1 = pw;
+            regionQueue.push([x0, y0, x1, y1]);
+        }
+    }
+
+    totalPixels = 0;
+    startTime = performance.now();
+    dispatchWorkers();
+}
+
+function dispatchWorkers()
+{
+    while (idleWorkers.length !== 0 && regionQueue.length !== 0)
+        plotRegionOnWorker(regionQueue.shift());
+}
+
+function plotRegionFinished(region, buffer, pixels)
+{
+    let [x0, y0, x1, y1] = region;
+    assert(x1 > x0 && y1 > y0, "plotRegionFinished got bad region");
+
+    let pw = x1 - x0;
+    let ph = y1 - y0;
+    assert(buffer.byteLength === pw * ph * 4, "Bad buffer size");
+
+    dispatchWorkers();
+
+    let colourData = new Uint8ClampedArray(buffer);
+
+    let image;
+    try {
+        image = new ImageData(colourData, pw, ph);
+    } catch (e) {
+        // This may not work in IE.
+        error("ImageData constructor not supported in your browser: " + e);
+    }
+
+    let context = plotterCanvas.getContext("2d");
+    context.putImageData(image, x0, y0);
+
+    totalPixels += pixels;
+
+    if (busyWorkers.length === 0) {
+        let endTime = performance.now();
+        plotterEndCallback(totalPixels, endTime - startTime);
+    }
+}
